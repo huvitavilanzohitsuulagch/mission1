3ad37c533c55b12babdcfb1f059f9019bec15d16
UnknownOne76 <ganboldgantor14@gmail.com>
2021-05-29 11:47:04 +0800

    Added by Gantur

--- engine.js
+++ engine.js
@@ -1,73 +1,60 @@
 let psl = document.getElementById("psl");
 let mvne = document.getElementById("mvn");
-let imb = document.getElementById("imb");
+let imb = document.getElementById("imd");
 let dsp = document.getElementById("dsc");
+let pstr = document.getElementById("poster");
+let db = firebase.firestore();
 
+db.collection('movies').onSnapshot(list => {
+    psl.value = "";
+    mvne.value = "";
+    dsp.value = "";
+    imb.value = "";
+    pstr.innerHTML = "";
+
+    // console.log(doc.data())
+    list.forEach((doc) => {
+        console.log(doc.data())
+        let postcontainer = document.createElement("div");
+        let posterlink = document.createElement("img");
+        let imdb = document.createElement("button");
+        let desc = document.createElement("div");
+        let infos = document.createElement("p");
+        infos.classList.add("para");
+        desc.appendChild(infos);
+        postcontainer.classList.add("poster-container")
+        posterlink.src = psl.value.trim();
+
+        posterlink.classList.add("poster")
+        imdb.classList.add("imbdlink")
+        imdb.innerHTML = "Link";
+        imdb.onclick = function() {
+            window.location.href = imb.value.trim();
+        }
+        desc.classList.add("movieinfo");
+        postcontainer.appendChild(posterlink);
+        postcontainer.appendChild(desc);
+        postcontainer.appendChild(imdb);
+        list.innerHTML = doc.data().list;
+        pstr.appendChild(postcontainer);
+    })
+})
 
 function res() {
-    let postcontainer = document.createElement("div");
-    let posterlink = document.createElement("img");
-    let imdb = document.createElement("button");
-    let desc = document.createElement("div");
-    let pstr = document.getElementById("poster");
-    postcontainer.classList.add("poster-container")
-    posterlink.src = psl.value.trim();
-
-    posterlink.classList.add("poster")
-    imdb.classList.add("imbdlink")
-    imdb.innerHTML = "Link";
-    imdb.onclick = function() {
-        window.location.href = imb.value.trim();
+    if (psl.value.trim() == "" && mvne.value.trim() == "" && imb.value.trim() == "") {
+        return 0;
     }
-    desc.classList.add("movieinfo")
-    postcontainer.appendChild(posterlink)
-    postcontainer.appendChild(desc)
-    postcontainer.appendChild(imdb)
-    pstr.appendChild(postcontainer)
+
+    db.collection('movies').add({
+        Postlink: psl.value.trim(),
+        MovieName: mvne.value.trim(),
+        Description: mvne.value.trim(),
+        IMDBLink: imb.value.trim(),
+    })
 }
 
-// function res() {
+// inp -> poster link , mname -> inp , imdb link -> inp , des -> inp , addls -> btn -> MAIN  , 
 
-//     let cont = document.createElement("div");
-//     let plink = document.createElement("img");
-//     let imdb = document.createElement("button");
-//     let hrd = document.createElement("div");
-//     hrd.classList.add("hold");
-//     let hr = document.createElement("hr");
-//     hr.classList.add("grn");
-//     let mvn = document.createElement("h3");
-//     mvn.classList.add("h3");
-//     mvn.innerHTML = mvne.value.trim();
-//     let desc = document.createElement("p");
-//     desc.classList.add("para");
-//     desc.innerHTML = dsp.value.trim();
+// Under the my list 
 
-//     let cn = document.createElement("div");
-//     cn.classList.add("cn");
-//     cn.appendChild(mvn);
-//     cn.appendChild(desc);
-//     hrd.appendChild(hr);
-
-//     plink.classList.add("plk");
-//     plink.src = psl.value.trim();
-//     imdb.innerHTML = "Link";
-//     imdb.classList.add("imdb");
-//     imdb.onclick = async function() {
-//         await console.log(imb.value.trim());
-//     }
-//     cont.appendChild(hrd);
-//     cont.appendChild(plink);
-//     cont.appendChild(cn);
-//     cont.appendChild(imdb);
-//     cont.classList.add("cnt");
-
-//     hd1.appendChild(cont);
-
-//     if (psl.value != "" && mvne.value != "" && imb.value != "" && dsp.value != "") {
-//         psl.value = "";
-//         mvne.value = "";
-//         imb.value = "";
-//         dsp.value = "";
-//     };
-
-// };
+// Needed codes: Document.createElement("img") , Document.createElement("button") , Document.createElement("div") , "div" -> appendChild ,
